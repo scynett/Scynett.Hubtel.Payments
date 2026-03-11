5f9aa13efd92ec7b2bbb6cdf7982ec1d64b40807
Obed Danso <obed.danso@scynett.com>
2026-03-11 11:32:48 +0000

    Added Comment to route

--- src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Status/RouteConstants.cs
+++ src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Status/RouteConstants.cs
@@ -1,6 +1,13 @@
 ﻿namespace Scynett.Hubtel.Payments.AspNetCore.DirectReceiveMoney.Status;
 
+/// <summary>
+/// Contains constant values for API routes used within the application.
+/// </summary>
 internal static class RouteConstants
 {
+    /// <summary>
+    /// The endpoint for receiving transaction status updates from Hubtel's 
+    /// Direct Receive Money service.
+    /// </summary>
     public const string TransactionStatus = "/hubtel/direct-receive-money/status";
 }

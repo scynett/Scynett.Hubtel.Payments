09630d250bde481fc5452fbc968fa8fde8e3fbfa
Obed Danso <obed.danso@scynett.com>
2026-03-11 11:13:25 +0000

    fix: Added a Comment on OnCompletedAsync Method

--- src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Callback/IReceiveMoneyCallbackHandler.cs
+++ src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Callback/IReceiveMoneyCallbackHandler.cs
@@ -11,6 +11,14 @@ namespace Scynett.Hubtel.Payments.AspNetCore.DirectReceiveMoney.Callback;
 /// </summary>
 public interface IReceiveMoneyCallbackHandler
 {
+    /// <summary>
+    /// Invoked when a "Receive Money" callback has been successfully processed.
+    /// </summary>
+    /// <param name="result">The result of the callback containing relevant information.</param>
+    /// <param name="cancellationToken">
+    /// A <see cref="CancellationToken"/> to observe while waiting for the task to complete.
+    /// </param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     Task OnCompletedAsync(
         ReceiveMoneyCallbackResult result,
         CancellationToken cancellationToken = default);

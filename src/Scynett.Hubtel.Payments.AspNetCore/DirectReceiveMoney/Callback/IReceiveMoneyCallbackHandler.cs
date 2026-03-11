dd6b997f4c66dd837dae1822dd84b166f5439a9e
samuelnettey <samuel.nettey@scynett.com>
2026-03-11 12:00:31 +0100

    fix : Hooked the callback endpoint into a new handler extension point so consuming apps can observe successful callbacks.

--- src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Callback/IReceiveMoneyCallbackHandler.cs
+++ src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Callback/IReceiveMoneyCallbackHandler.cs
@@ -7,6 +7,7 @@ namespace Scynett.Hubtel.Payments.AspNetCore.DirectReceiveMoney.Callback;
 
 /// <summary>
 /// Allows consuming applications to react to successfully processed callbacks.
+/// Hooked the callback endpoint into a new handler extension point so consuming apps can observe successful callbacks.
 /// </summary>
 public interface IReceiveMoneyCallbackHandler
 {

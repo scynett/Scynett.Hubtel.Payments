30a48b79d158206ce381678a3ce9820318908d26
Obed Danso <obed.danso@scynett.com>
2026-03-11 11:33:30 +0000

    fix: small change in spaces

--- src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Status/RouteConstants.cs
+++ src/Scynett.Hubtel.Payments.AspNetCore/DirectReceiveMoney/Status/RouteConstants.cs
@@ -7,7 +7,7 @@ internal static class RouteConstants
 {
     /// <summary>
     /// The endpoint for receiving transaction status updates from Hubtel's 
-    /// Direct Receive Money service.
+    /// Direct Receive Money  service.
     /// </summary>
     public const string TransactionStatus = "/hubtel/direct-receive-money/status";
 }
